17f60f45e46e537c6e89b83724509eece1e10348
Егор Сивцев <gumben7@gmail.com>
2023-03-09 15:15:17 +0700

    Car is realized

--- Car.cs
+++ Car.cs
@@ -1,5 +1,16 @@
 namespace ParkingLot {
     class Car {
-        
+        private string _number;
+        private string _color;
+        public string Number {
+            init => this._number = value; 
+            get => this._number;
+        }
+        public string Make {init; get;}
+        public string Model {init; get;}
+        public string Color {
+            init => this._color = value; 
+            get => this._color;
+        }
     }
 }
