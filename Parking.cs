5736f0364e4003acf087421488a9be8d0817f600
Егор Сивцев <gumben7@gmail.com>
2023-03-09 16:13:31 +0700

    prefinal

--- Parking.cs
+++ Parking.cs
@@ -1,5 +1,36 @@
 namespace ParkingLot {
     class Parking {
+        private Dictionary<string, ParkingRecord> _cars = new Dictionary<string, ParkingRecord>();
+        public void Accommodate(Car car) {
+            this._cars.Add(car.Number, new ParkingRecord(car, DateTime.Now));
+        }
+        public void TurnOut(Car car) {
+            this.TurnOutTheCarWithNumber(car.Number);
+        }
+        public void TurnOutTheCarWithNumber(string number) {
+            var parkedDateTime = this._cars[number].ParkedDateTime;
+            var turnedOutDateTime = DateTime.Now;
+            Console.WriteLine($"Машина с номером {number} вывезена");
+            Console.WriteLine($"Время прибытия: {parkedDateTime}");
+            Console.WriteLine($"Время убытия: {turnedOutDateTime}");
+            Console.WriteLine($"Простояла {(turnedOutDateTime - parkedDateTime).Hours} часов и {(turnedOutDateTime - parkedDateTime).Minutes % 60} минуту");
+            this._cars.Remove(number);
+        }
+        public int CarsCount() => this._cars.Count;
 
+        public List<string> CarList() {
+            return new List<string>(this._cars.Keys);
+        }
+
+        public Car GetCarByNumber(string number) => this._cars[number].ParkedCar;
+    }
+
+    struct ParkingRecord {
+        public ParkingRecord(Car car, DateTime dateTime) {
+            this.ParkedCar = car;
+            this.ParkedDateTime = dateTime;
+        }
+        public Car ParkedCar {init; get;}
+        public DateTime ParkedDateTime {init; get;}
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
     class Program {
         static void Main(string[] args) {
             var pL = new Parking();
+            pL.Accommodate(new Car() {Number = "123"});
+            pL.Accommodate(new Car() {Number = "456"});
+            pL.Accommodate(new Car() {Number = "789"});
+            foreach (var c in pL.CarList()) {
+                Console.WriteLine(c);
+            }
         }
     }
 }
